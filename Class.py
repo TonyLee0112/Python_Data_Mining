de3636917a416635809e3171c89a01469a36bd8b
TonyLee0112 <115335757+TonyLee0112@users.noreply.github.com>
2025-05-03 12:14:57 +0900

    Create Class.py

---
+++ Class.py
@@ -0,0 +1,100 @@
+class Human :
+    __count = 0
+
+    def __init__(self, name, age=0, gender=1):
+        self.__name = name
+        self.__age = age
+        self.__gender = gender
+        Human.inc_count()
+    def __del__(self):
+        Human.dec_count()
+
+    def setgender(self,gender):
+        self.__gender = gender
+    def getgender(self):
+        return self.__gender
+
+    def setname(self, name) :
+        self.__name = name
+    def getname(self):
+        return self.__name
+
+    def setage(self, age) :
+        self.__age = age
+    def getage(self):
+        return self.__age
+
+    def __whatisgender(self):
+        if self.__gender == 1:
+            return "남성"
+        else :
+            return "여성"
+    def view(self):
+        print("%10s %10d % 10s"%(self.__name, self.__age, self.__whatisgender()))
+
+    @classmethod
+    def inc_count(cls):
+        cls.__count += 1
+    @classmethod
+    def dec_count(cls):
+        cls.__count -= 1
+    @classmethod
+    def getcount(cls):
+        return cls.__count
+
+humanList = []
+
+def humanAdd():
+    info = input("이름 나이 성별을 입력하세요. : ").split()
+    print("1: 남성, 2: 여성")
+    instance = Human(info[0],int(info[1]),int(info[2]))
+    humanList.append(instance)
+
+def allView():
+    print()
+    print("==========================")
+    print("%10s %10s %10s"%("이름","나이","성별"))
+    print("==========================")
+
+    for h in humanList :
+        h.view()
+
+
+def humanDel():
+    allView()
+    name = input("삭제할 사람의 이름을 입력하세요 : ")
+    isFind = False
+    for idx in range(len(humanList)) :
+        if name == humanList[idx].getname() :
+            isFind = True
+            break
+    if isFind == True :
+        del humanList[idx] # 이게 되나?
+    else :
+        print("입력한 이름을 가진 사람이 리스트에 없습니다.")
+
+
+
+def menuView() :
+    print("%15s" % "Menu")
+    print("==========================")
+    print("1 : 회원 등록")
+    print("2 : 회원 삭제")
+    print("3 : 전체 회원 리스트 조회")
+    print("4 : 프로그램 종료")
+    print("==========================")
+
+# Main()
+while True :
+    menuView()
+    num = int(input("Please choose a number : "))
+    if num == 4 :
+        break
+    elif num == 1 :
+        humanAdd()
+    elif num == 2 :
+        humanDel()
+    elif num == 3 :
+        allView()
+    else :
+        print("잘못된 입력입니다.\n다시 입력하세요.")
