25b65e2b101a18c9383c5d40e4a78aca11ea2238
TonyLee0112 <115335757+TonyLee0112@users.noreply.github.com>
2025-05-14 14:58:14 +0900

    Create Pandas.py

---
+++ Pandas.py
@@ -0,0 +1,16 @@
+import pandas as pd
+# Pandas 는 서로 다른 유형들의 데이터들을 공통의 포맷으로 정리하는 것.
+# 여러 포맷들을 하나로 통일하기 위해 Series 와 DataFrame 이라는 구조화된 data type 을 제공함.
+# Series : 1D array 형태로, index 와 data value 는 일대일로 대응이 되는 구조. dictionary 와 list 를 series 로 변환가능
+# DataFrame : 여러 개의 Series 들의 집합체.
+
+df = pd.DataFrame([
+    ['Apple', 7, 'Fruit'],
+    ['Banana',3,'Fruit'],
+    ['Beef',5,'Meal'],
+    ['Beer',4,'Meal']],
+    columns=['Name','Frequency','Type']
+)
+
+print(df)
+print(df.groupby(['Type']).sum())
